0e64849153190936530c6edd2515e33a65b1ae3e
MeiNanita <Mei-Nanita@gmx.net>
2024-10-08 15:24:09 +0200

    added new primitive-era quests to the questbook

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -1,7 +1,7 @@
 JEIEvents.hideItems(event => {
 
-    event.hide('#notreepunching:knives');
-    event.hide('#notreepunching:saws');
+    //event.hide('#notreepunching:knives');
+    //event.hide('#notreepunching:saws');
     event.hide('#notreepunching:mattocks');
 
 });
