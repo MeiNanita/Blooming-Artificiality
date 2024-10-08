111487358b9421aa33196f53ab017e60844387ab
MeiNanita <Mei-Nanita@gmx.net>
2024-10-08 10:37:51 +0200

    code-cleanup and removal of exnihilo

--- kubejs/server_scripts/crafting/permanent_removals.js
+++ kubejs/server_scripts/crafting/permanent_removals.js
@@ -1,7 +1,8 @@
 ServerEvents.recipes(event => {
-    event.remove({output: '#exnihilosequentia:hammer'});
-    event.remove({output: '#exnihilosequentia:crook'});
+    event.remove({output: '#notreepunching:knives'});
+    event.remove({id: 'notreepunching:plant_string'});
+    event.remove({id: 'minecraft:crafting_table'});
 
-    event.remove({input: 'gtceu:impure_hematite_dust', type: 'minecraft:smelting'});
-    event.remove({input: 'gtceu:hematite_dust', type: 'minecraft:smelting'});
+    //event.remove({input: 'gtceu:impure_hematite_dust', type: 'minecraft:smelting'});
+    //event.remove({input: 'gtceu:hematite_dust', type: 'minecraft:smelting'});
 });
--- kubejs/server_scripts/crafting/primitive_crafting.js
+++ kubejs/server_scripts/crafting/primitive_crafting.js
@@ -1,13 +1,21 @@
 
 ServerEvents.recipes(event => {
 
-    event.remove({output: 'minecraft:crafting_table'});
     event.shaped(Item.of('minecraft:crafting_table'), [
         'AK',
         'WW'
     ], {
         W: '#minecraft:logs',
         A: 'notreepunching:flint_axe',
+        K: 'gtceu:flint_knife'
+    });
+
+    event.shaped(Item.of('minecraft:crafting_table'), [
+        'AK',
+        'WW'
+    ], {
+        W: '#minecraft:logs',
+        A: '#forge:tools/axes',
         K: '#forge:tools/knives'
     });
 
@@ -20,70 +28,6 @@ ServerEvents.recipes(event => {
         'minecraft:iron_ingot',
         'gtceu:wrought_iron_plate'
     );
-
-    event.shaped(Item.of('exnihilosequentia:oak_sieve'), [
-        'P P',
-        'PHP',
-        'S S'
-    ], {
-        P: 'biomesoplenty:dead_planks',
-        H: 'biomesoplenty:dead_slab',
-        S: '#forge:rods/wood'
-    });
-
-    event.remove({output: 'exnihilosequentia:flint_mesh'});
-    event.remove({output: 'exnihilosequentia:iron_mesh'});
-    event.remove({output: 'exnihilosequentia:diamond_mesh'});
-    event.remove({output: 'exnihilosequentia:emerald_mesh'});
-    event.remove({output: 'exnihilosequentia:netherite_mesh'});
-
-    event.shaped(Item.of('exnihilosequentia:flint_mesh'), [
-        'PMP',
-        'MGM',
-        'PMP'
-    ], {
-        P: 'gtceu:flint_dust',
-        G: 'gtceu:flint_block',
-        M: 'exnihilosequentia:string_mesh'
-    });
-
-    event.shaped(Item.of('exnihilosequentia:iron_mesh'), [
-        'PMP',
-        'MGM',
-        'PMP'
-    ], {
-        P: 'gtceu:fine_steel_wire',
-        G: 'gtceu:steel_frame',
-        M: 'exnihilosequentia:flint_mesh'
-    });
-
-    event.remove({output: 'immersive_weathering:plate_iron'});
-    event.shaped(Item.of('immersive_weathering:plate_iron', 4), [
-        'BB',
-        'BB'
-    ], {
-        B: 'minecraft:iron_block'
-    });
-
-    event.shaped(Item.of('xkdeco:factory_block_rusted', 3), [
-        'SS',
-        'SS'
-    ], {
-        S: 'xkdeco:factory_stairs_rusted'
-    });
-
-    event.shaped(Item.of('xkdeco:factory_block_rusted'), [
-        'S',
-        'S'
-    ], {
-        S: 'xkdeco:factory_slab_rusted'
-    });
-
-    event.shapeless(Item.of('xkdeco:factory_slab_rusted'), [
-        'xkdeco:factory_trapdoor_rusted',
-        'xkdeco:factory_trapdoor_rusted',
-        'xkdeco:factory_trapdoor_rusted'
-    ]);
 
 
     // ===================================================================
@@ -164,6 +108,14 @@ ServerEvents.recipes(event => {
         'minecraft:cobblestone', 
         [
             Item.of('kubejs:stone_pebble', 9)
+        ]
+    );
+
+
+    event.shapeless(
+        'notreepunching:plant_string', 
+        [
+            Item.of('notreepunching:plant_fiber', 4)
         ]
     );
 
